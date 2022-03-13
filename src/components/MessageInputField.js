fdf64cafb0dd2d973035e39c27876812b5295f0d
tobeken <tobik0124@gmail.com>
2022-03-13 13:46:23 +0900

    gravatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Grid, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
+import { gravatarPath } from "../gravatar";
+
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
     margin: "26px",
   },
 });
-const MessageInputField = () => {
+const MessageInputField = ({ name }) => {
   const classes = useStyles();
+  const avatarPath = gravatarPath(name);
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid xs={1}>
-          <Avatar />
+          <Avatar src={avatarPath} />
         </Grid>
         <Grid xs={10}>入力</Grid>
         <Grid xs={1}>Button</Grid>
