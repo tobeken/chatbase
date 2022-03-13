014137f6cef151f68473fc9a2e991b38bf66989b
tobeken <tobik0124@gmail.com>
2022-03-13 21:23:18 +0900

    firebase env config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from "firebase";
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_API_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCR9iampZPpPjNeQ7Xvy2KrGpphmNRnGws",
-  authDomain: "chatbase-5b8c8.firebaseapp.com",
-  databaseURL: "https://chatbase-5b8c8-default-rtdb.firebaseio.com",
-  projectId: "chatbase-5b8c8",
-  storageBucket: "chatbase-5b8c8.appspot.com",
-  messagingSenderId: "181783479151",
-  appId: "1:181783479151:web:cc73dd3d32fc1180ca5e9a",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_API_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
