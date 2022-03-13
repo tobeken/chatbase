610ac87a2c303d657177092f2ed9688e27c722f9
tobeken <tobik0124@gmail.com>
2022-03-13 10:00:13 +0900

    set your name

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+export default function SignIn({ setName }) {
   const classes = useStyles();
 
   return (
